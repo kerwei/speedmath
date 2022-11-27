aec6d8dbc12da78cd203ef5c75d6fa4c3f66a76d
kerwei <kerwei84@gmail.com>
2022-11-27 21:42:22 +0800

    Parameterize difficulty and intensity

--- main.cpp
+++ main.cpp
@@ -4,14 +4,31 @@
 using namespace std;
 
 
-int main() {
-    // Create a new game
-    Manager gameManager = Manager(1, 1);
+int main(int argc, char* argv[]) {
+    // Set difficulty
+    std::string sdiff{""};
+    std::cout << "Enter the difficulty: (1) 1-digit (2) 2-digits ..." << std::endl;
+    std::cin >> sdiff;
 
-    try {
-        std::cout << gameManager.qnext() << std::endl;
+    // Set intensity (number of questions)
+    std::string sintense{""};
+    std::cout << "Enter the intensity: (1) 10 questions (2) 20 questions ..." << std::endl;
+    std::cin >> sintense;
+
+    int diff{stoi(sdiff)};
+    int intense{stoi(sintense)};
+
+    // Create a new game
+    Manager gameManager = Manager(diff, intense);
+
+    while (true) {
+        try {
+            std::cout << gameManager.qnext() << std::endl;
+        }
+        catch (const std::out_of_range& e) {
+            break;
+        }
     }
-    catch (const std::out_of_range& e) {}
 
     return 0;
 }
